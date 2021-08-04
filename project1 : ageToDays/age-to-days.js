25bc1afdd4c27dba505185c91396786fd6b23654
Michaelexe <michaelispahani@gmail.com>
2021-08-05 00:24:24 +0430

    first thing i made with js and html

---
+++ project1 : ageToDays/age-to-days.js
@@ -0,0 +1,9 @@
+
+function ageInDays() {
+	var age = parseInt(prompt('What is your age?'));
+	document.getElementById('yourAge').innerHTML = "your age in days is " + (age * 365).toString();
+}
+
+function reset() {
+	document.getElementById('yourAge').innerHTML = '';
+}
