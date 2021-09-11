a92acf5a26fc0dbbbf472a0818f6b135c4279b06
Michaelexe <michaelispahani@gmail.com>
2021-09-11 15:20:52 +0430

    languageGuesser

---
+++ languageGuesser/index.js
@@ -0,0 +1,21 @@
+import {franc} from "franc"
+import langs from "langs"
+
+//you can't use require in a  type set as module if you don't define require.
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+var colors = require('colors');
+
+const text = process.argv.slice(2)
+
+for (let string of text) {
+    const languageCode = franc(string)
+    const languageObject = langs.where('3', languageCode)
+    if (languageObject) {
+        console.log(languageObject.name.green)
+    } else {
+        console.log('Language could not be detected'.red)
+    }
+
+
+}
