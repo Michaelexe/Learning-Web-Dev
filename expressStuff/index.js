c79c8df391619291652e84cde430889d9f76babd
Michaelexe <michaelispahani@gmail.com>
2021-09-13 02:20:38 +0430

    express

--- expressStuff/index.js
+++ expressStuff/index.js
@@ -1,14 +1,18 @@
+//importing whatever we need
 const express = require('express');
 const app = express()
 const path = require('path')
 
+//setting engines and the views folder directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
+//gets requests and responses from the root url
 app.get('/', (req,res) => {
-    res.render('home')
+    res.render('home') //rendering the ejs file in the views folder
 })
 
+//gets requests and responses from the /r/(variable) url
 app.get('/r/:subreddit', (req, res)=> {
     const { subreddit } = req.params
     res.render('subreddit', { subreddit })
