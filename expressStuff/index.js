53b67bc7186998b456d12841f1384e1c20ee8e62
Michaelexe <michaelispahani@gmail.com>
2021-09-12 23:08:23 +0430

    express

--- expressStuff/index.js
+++ expressStuff/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
 
 app.listen(3000, ()=>{ //3000 is the port
-    console.log('wassup')
+    console.log('LISTENING ON PORT 3000')
 })
