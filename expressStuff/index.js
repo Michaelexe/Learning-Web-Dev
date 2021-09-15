0687f46b61ebb9981e76dbd22bf178abc7dda39a
Michaelexe <michaelispahani@gmail.com>
2021-09-16 00:50:11 +0430

    modified

--- expressStuff/index.js
+++ expressStuff/index.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const app = express()
 const path = require('path')
+const redditData = require('./data.json')
 
 //setting engines and the views folder directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
-app.use(express.static('public'))
+app.use(express.static('public')) //static fo;es folder name
+
 
 //gets requests and responses from the root url
 app.get('/', (req,res) => {
@@ -16,7 +18,9 @@ app.get('/', (req,res) => {
 //gets requests and responses from the /r/(variable) url
 app.get('/r/:subreddit', (req, res)=> {
     const { subreddit } = req.params
-    res.render('subreddit', { subreddit })
+    if (redditData[subreddit]) {
+        res.render('subreddit', { subreddit, subredditData : redditData[subreddit] })
+    }
 })
 
 app.listen(3000, ()=>{ //3000 is the port
